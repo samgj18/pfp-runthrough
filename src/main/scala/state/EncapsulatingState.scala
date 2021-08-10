5c48fe672b5fd833c4381cb532e1ada0a4854d28
Samuel Gómez <samgj_18@hotmail.com>
2021-08-10 14:16:45 -0500

    Create EncapsulatingState.scala

---
+++ src/main/scala/state/EncapsulatingState.scala
@@ -0,0 +1,14 @@
+package state
+
+// Let's say we have an in-memory counter
+
+object EncapsulatingState {
+  // One of the best approaches to managing state is to encapsulate state in the
+  // interpreter and only expose an abstract interface with the functionality the
+  // user needs.
+
+  /**
+    * OUR INTERFACE SHOULD KNOW NOTHING ABOUT STATE
+    */
+
+}
